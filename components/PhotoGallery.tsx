dae4bdc0ece8e9eee1bf7cf92c0718b7bf358272
lordshy <majestic.lordshy@gmail.com>
2026-02-13 21:39:49 +0700

    Add files via upload

---
+++ components/PhotoGallery.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Component removed as per user request to simplify the app
+const PhotoGallery: React.FC = () => {
+  return null;
+};
+
+export default PhotoGallery;
