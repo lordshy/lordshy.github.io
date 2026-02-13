0d4d7354087758052bec00f43f8281087f58fab4
lordshy <majestic.lordshy@gmail.com>
2026-02-13 21:37:12 +0700

    Delete components directory321321
    321312

--- components/PhotoGallery.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Component removed as per user request to simplify the app
-const PhotoGallery: React.FC = () => {
-  return null;
-};
-
-export default PhotoGallery;
